63cea261ac6632a7c50942bbbdf2dbc0fc6a2383
emmanuel.maravilla76 <emmanuel.maravilla76@gmail.com>
2018-03-27 17:29:48 -0400

    midterm

---
+++ Midterm.java
@@ -0,0 +1,31 @@
+/* 
+  Emmanuel Maravilla
+  Co Sci290
+  3/22/18
+  
+  Comments: The following code will ask the user for a 5 digit number then
+            will determine whether the number inputed is a palindrome.
+  
+ */
+ 
+ 
+  import java.util.Scanner;
+    
+    public class Midterm{
+      
+      public static void main(String[] args){
+      
+      Scanner input = new Scanner(System.in);
+      
+      System.out.println("Enter a five digit integer: ");
+        
+        int palin = input.nextInt();
+        
+        int palin1 = (int)(palin / 100);
+        int remaining = palin % 100;
+        int palin3 = (int)(remaining % 10);
+        
+      
+      System.out.println(palin + ((palin1 == palin3) ? " is a " : " is not a ") + "palindrome");
+      }
+    }
